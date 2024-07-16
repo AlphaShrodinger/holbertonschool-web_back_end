b6d176aaf67f6acbcca4068edc975ec77e767f6b
jose <joseoquendovazquez@gmail.com>
2024-07-16 17:53:44 -0400

    attempt 6

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,8 +1,4 @@
-function getSumOfHoods(
-    initialNumber,
-    expansion1989 = 89,
-    expansion2019 = 19,
-) {
+function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
     return initialNumber + expansion1989 + expansion2019;
 }
 
