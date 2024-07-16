6b208e36c5a687709ae762126b9f237390269a00
jose <joseoquendovazquez@gmail.com>
2024-07-16 17:41:41 -0400

    task 3

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,9 @@
+function getSumOfHoods(
+    initialNumber = 0,
+    expansion1989 = 89,
+    expansion2019 = 19,
+) {
+    return initialNumber + expansion1989 + expansion2019;
+}
+
+module.exports = getSumOfHoods;
