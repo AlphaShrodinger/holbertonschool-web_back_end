86cd16169907162e1548d6d46c01e272696499ff
jose <joseoquendovazquez@gmail.com>
2024-07-19 11:59:30 -0400

    task 7 attempt 2

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,15 +1,10 @@
 export default class Airport {
-    /**
-       *
-       * @param {String} name
-       * @param {String} code
-       */
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    toString() {
-        return `[object ${this._code}]`;
-    }
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
